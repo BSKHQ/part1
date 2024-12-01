7a36e54e51ffb6b1abe2c4d818257db2be1ded29
BSKHQ <kunlebakaresodiq@gmail.com>
2024-12-01 17:43:51 +0100

    exercise 1.1

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -12,12 +12,30 @@ const App = () => {
 
   return (
     <div>
-      <h1> {course}</h1>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises = {exercises1}/>
+      <Content part={part2} exercises = {exercises2}/>
+      <Content part={part3} exercises = {exercises3}/>
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
     </div>
+  )
+}
+
+const Header = (props) => {
+  return(
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
   )
 }
 
